e8cbc01f9f4b602dd0fe1e495720543fb6b6fc50
seven316 <xseven316@gmail.com>
2021-08-07 21:52:07 -0600

    Agregar archivos de proyecto.

---
+++ CarritoCompras/CarritoCompras/AppSettings.cs
@@ -0,0 +1,28 @@
+﻿using AppNotas.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarritoCompras
+{
+    public class AppSettings
+    {
+        public static string ApiFirebase = "https://proyecto-final-ffc8f-default-rtdb.firebaseio.com/";
+        private static string KeyAplication = "AIzaSyAKo_qGJyKI57qN8_r7CX3KDWSPHA1Jmbw";
+
+
+        public static ResponseAuthentication oAuthentication { get; set; }
+        private static string ApiUrlGoogleApis = "https://identitytoolkit.googleapis.com/v1/";
+
+        public static string ApiAuthentication(string tipo)
+        {
+            if (tipo == "LOGIN")
+                return ApiUrlGoogleApis + "accounts:signInWithPassword?key=" + KeyAplication;
+            else if (tipo == "SIGNIN")
+                return ApiUrlGoogleApis + "accounts:signUp?key=" + KeyAplication;
+            else
+                return "";
+        }
+
+    }
+}
